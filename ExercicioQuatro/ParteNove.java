167ad6c6122009c5311a01ab3253f37ed80b0d4e
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2021-10-04 12:56:00 -0300

    Arrumando código
    Arrumando código

--- ExercicioQuatro/ParteNove.java
+++ ExercicioQuatro/ParteNove.java
@@ -1,5 +1,5 @@
-package ExercicioQuatro;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ParteNove {
     // 9) Crie dois arrays de 10 posições e depois gere um terceiro array com os valores intercalados desses dois arrays, ao final, imprima os 3. 
@@ -8,19 +8,21 @@ public class ParteNove {
         int qtd = 10;
         int[] array1 = new int[qtd];
         int[] array2 = new int[qtd];
-        int[] array3 = new int[qtd];
+        int lenArray1 = array1.length;
+        int lenArray2 = array2.length;
+        int[] array3 = new int[lenArray1 + lenArray2];
 
         for(int i=0; i<qtd; i++) {
             System.out.println(" Digite o "+(1+i)+"o valor do primeiro array: ");
             array1[i] = print.nextInt();
-            array3[i] = array1[i];
             System.out.println(" Digite o "+(1+i)+"o valor do segundo array: ");
             array2[i] = print.nextInt();
-            array3[10+i] = array2[i];
         }
-        for(int i = 0; i< qtd + qtd; i++) {
-            System.out.println((i+1)+"a número: "+array3[i]); 
-        }
+        System.arraycopy(array1, 0, array3, 0, lenArray1);  
+        System.arraycopy(array2, 0, array3, lenArray1, lenArray2);  
+        System.out.println("Array1: " + Arrays.toString(array1));
+        System.out.println("Array2: " + Arrays.toString(array2));
+        System.out.println("Concatenated Array: " + Arrays.toString(array3)); 
         print.close();
     }
 }
